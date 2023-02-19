0ca916f4420acecf54604aad9a790279f73e739c
Ayrat Agrulov <lizardrock@mail.ru>
2023-02-19 20:55:01 +0300

    db updated

---
+++ HITteamBot/Migrations/20230219175150_newTables.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HITteamBot.Migrations
+{
+    public partial class newTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EquipmentId",
+                table: "Character");
+
+            migrationBuilder.CreateTable(
+                name: "Ammo",
+                columns: table => new
+                {
+                    Id = table.Column<short>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeId = table.Column<short>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ammo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Armor",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArmorTypeId = table.Column<short>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    DefenceValue = table.Column<short>(nullable: false),
+                    RarityId = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Armor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArmorTypes",
+                columns: table => new
+                {
+                    Id = table.Column<short>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArmorTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chemicals",
+                columns: table => new
+                {
+                    Id = table.Column<short>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chemicals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Explne",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Level = table.Column<short>(nullable: false),
+                    NecessaryExp = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Explne", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InventoryItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CharacterId = table.Column<long>(nullable: false),
+                    ItemId = table.Column<short>(nullable: false),
+                    ItemTypeId = table.Column<short>(nullable: false),
+                    Count = table.Column<short>(nullable: false),
+                    IsEquiped = table.Column<bool>(nullable: false),
+                    BoostId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemTypes",
+                columns: table => new
+                {
+                    Id = table.Column<short>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Junk",
+                columns: table => new
+                {
+                    Id = table.Column<short>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Junk", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChatId = table.Column<long>(nullable: false),
+                    UserId = table.Column<long>(nullable: false),
+                    Username = table.Column<string>(nullable: true),
+                    CharacterId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weapon",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WeaponTypeId = table.Column<short>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    AmmoId = table.Column<short>(nullable: false),
+                    Damage = table.Column<int>(nullable: false),
+                    FireRate = table.Column<short>(nullable: false),
+                    FireDistance = table.Column<short>(nullable: false),
+                    IsHeavy = table.Column<bool>(nullable: false),
+                    RarityId = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weapon", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeaponTypes",
+                columns: table => new
+                {
+                    Id = table.Column<short>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeaponTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ammo");
+
+            migrationBuilder.DropTable(
+                name: "Armor");
+
+            migrationBuilder.DropTable(
+                name: "ArmorTypes");
+
+            migrationBuilder.DropTable(
+                name: "Chemicals");
+
+            migrationBuilder.DropTable(
+                name: "Explne");
+
+            migrationBuilder.DropTable(
+                name: "InventoryItems");
+
+            migrationBuilder.DropTable(
+                name: "ItemTypes");
+
+            migrationBuilder.DropTable(
+                name: "Junk");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Weapon");
+
+            migrationBuilder.DropTable(
+                name: "WeaponTypes");
+
+            migrationBuilder.AddColumn<long>(
+                name: "EquipmentId",
+                table: "Character",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
