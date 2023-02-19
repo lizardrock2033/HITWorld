0b6712a8babcecbe5d1a4858b50086137196479d
Airat Agrulov <lizardrock2033@gmail.com>
2023-02-19 19:18:04 +0300

    ы

---
+++ HITteamBot/Repository/Domain/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using HITteamBot.Repository.Entities.Characters;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HITteamBot.Repository.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<SPECIAL> SPECIAL { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=sql.bsite.net\MSSQL2016;User ID=lizardrock_FaTDB;Password=88005553535Qwa;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
